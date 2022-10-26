22ef1ef3aa1e19a406ee04fa1910d5c74f8a1d03
papakwesi <93794323+Papakwesi@users.noreply.github.com>
2022-10-27 04:41:37 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,22 @@
+// nav
+
+$('<form action="#"><select /></form>').appendTo("#nav");
+$("<option />",{selected:"selected",value:"",text:"MENU"}).appendTo("#nav select");
+$("#nav a").each(function(){var e=$(this);
+if($(e).parents("ul ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"> > "+e.text()}).appendTo("#nav select")}
+	else if($(e).parents("ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - "+e.text()}).appendTo("#nav select")}
+		else if($(e).parents("ul").length>=1){$("<option />",{value:e.attr("href"),text:"|| "+e.text()+" ||"}).appendTo("#nav select")}
+			else{$("<option />",{value:e.attr("href"),text:e.text()}).appendTo("#nav select")}});
+$("#nav select").change(function(){if($(this).find("option:selected").val()!=="#"){window.location=$(this).find("option:selected").val()}})
+
+
+// update
+
+function DisplayAlert() {
+  var newLine = "\r\n"
+  var msg = "Loan repricing FAQ's added"
+  msg += newLine;
+  msg += "";
+  msg += newLine;
+  alert(msg);
+}
